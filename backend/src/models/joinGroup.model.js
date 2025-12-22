ce7b97ea83b74c71c8afeffbaddd5be901360e20
kiprudroid <kiprutoshadrack06@gmail.com>
2025-12-23 00:51:26 +0300

    Update join group and SCC files

---
+++ backend/src/models/joinGroup.model.js
@@ -0,0 +1,35 @@
+import db from "../config/db.config.js";
+
+export const createJoinGroups = async (data) => {
+    const { full_name, phone_number, email, gender, college, group_joined } = data; 
+    const query = `
+    INSERT INTO groups (full_name, phone_number, email, gender, college, group_joined)
+    VALUES ($1, $2, $3, $4, $5, $6)
+    RETURNING *
+    `;
+
+    const values = [full_name, phone_number, email, gender, college, group_joined];
+    const result = await db.query(query, values);
+
+    return result.rows[0];
+};
+
+export const getJoinGroups = async () => {
+    try {
+        const result = await db.query(
+        "SELECT * FROM groups ORDER BY full_name ASC"
+    );
+    return result.rows;
+    } catch (error) {
+        console.log("Database Error " + error.message);
+    }
+
+};
+
+export const deleteJoinGroups = async (id) => {
+    const query = `
+    DELETE FROM groups WHERE user_id = $1
+    `;
+    const values = [id];
+    await db.query(query, values);
+};
---
+++ backend/src/services/joinGroup.service.js
@@ -0,0 +1,18 @@
+import * as joinGroupModel from "../models/joinGroup.model.js";
+
+export const createJoinGroupsServices = async (data) => {
+    if (!data) {
+        throw new Error("No data provided");
+    }   
+    if (!data.full_name || !data.phone_number || !data.email || !data.gender || !data.college || !data.group_joined) {
+        throw new Error("Missing required fields");
+    }
+    return await joinGroupModel.createJoinGroups(data);
+}
+export const getJoinGroupsServices = async () => {
+    return await joinGroupModel.getJoinGroups();
+}
+
+export const deleteJoinGroupServices = async (id) => {
+    return await joinGroupModel.deleteJoinGroups(id);
+}
