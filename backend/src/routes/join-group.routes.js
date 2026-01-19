d2ccd39f434a2eac02d58a7105d273276d0107d8
kilibanks <wanmun3456@gmail.com>
2026-01-19 15:27:20 +0300

    routes modified

--- backend/src/routes/join-group.routes.js
+++ backend/src/routes/join-group.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import joinGroupsController from "../controllers/join-roup.controller.js";
+import joinGroupsController from "../controllers/join-group.controller.js";
 
 const router = Router();
 
